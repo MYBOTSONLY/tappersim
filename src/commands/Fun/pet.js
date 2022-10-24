64c8d8acb4d8fe2fc3be1ce2f911b2072960d9b4
MYBOTSONLY <95443683+MYBOTSONLY@users.noreply.github.com>
2022-10-24 07:58:22 +0100

    Update pet.js

--- src/commands/Fun/pet.js
+++ src/commands/Fun/pet.js
@@ -1,11 +1,11 @@
 module.exports = {
-    name: 'Pet',
+    name: 'pet',
     category: 'Fun',
     description: 'Find Pet Values!',
     options: [
         {
             type: 6,
-            name: 'Pet Name',
+            name: 'petname',
             focused: true,
             description: 'What is the pet name?',
             required: true
@@ -18,9 +18,9 @@ module.exports = {
                 content: ``` __**Sour Slug**__
 (LEGENDARY)
 
-Type: Normal ┃ Price: 5M:gem: ┃ Demand: 10/10
-Type: Shiny:sparkles: ┃ Price: 20M:gem: ┃ Demand: 10/10
-Type: Rainbow:rainbow: ┃ Price: 50M:gem: ┃ Demand: 10/10```,
+Type: Normal â”ƒ Price: 5M:gem: â”ƒ Demand: 10/10
+Type: Shiny:sparkles: â”ƒ Price: 20M:gem: â”ƒ Demand: 10/10
+Type: Rainbow:rainbow: â”ƒ Price: 50M:gem: â”ƒ Demand: 10/10```,
                 ephemeral: false
             });
         }
